1ae4afa55cd707719c96d15a30d0304eaf2ae26e
Christian Stewart <heeler_cs@hotmail.com>
2023-03-29 20:51:11 -0500

    Better logging, setInterval(), fps

--- main.js
+++ main.js
@@ -3,49 +3,51 @@ import { getDatabase, ref, push } from 'firebase/database'
 
 import { poseData } from './pose-data.js'
 import { firebaseConfig } from './firebase-config.js'
+import { convertTimeTextToMs } from './convertTimeTextToMs.js'
+import { saveLog } from './logger.js'
+
+const runTimeString = '1s'
+const targetFps = 30
 
 const app = initializeApp(firebaseConfig)
+const db = getDatabase()
 
-const writeUserData = async () => {
-	const db = getDatabase()
-	const totalFramesSent = 30
-	const results = []
+const writeUserData = (targetFps, runTimeString) => {
+	const runTimeMs = convertTimeTextToMs(runTimeString)
+	const totalFramesToSend = targetFps * (runTimeMs / 1000)
+	const promises = []
+	let counter = 0
+	let end1
 
+	console.log('Sending data...')
 	const start = Date.now()
-	for (let i = 0; i < totalFramesSent; i++) {
-		results.push(push(ref(db, 'poseData/'), poseData))
-	}
-	await Promise.allSettled(results)
-	const end = Date.now()
+	const intervalId = setInterval(async () => {
+		promises.push(push(ref(db, 'poseData/'), poseData))
+		counter++
 
-	const deltaSec = (end - start) / 1000
-
-	const data = JSON.stringify(poseData)
-	const dataBlob = new Blob([data], { type: 'application/json' })
-
-	const singleFrameInBytes = dataBlob.size
-	const maxFramesPerSec = 30
-	const maxFrameSizePerSec = singleFrameInBytes * maxFramesPerSec
-
-	const sizeDataSentInBytes = totalFramesSent * singleFrameInBytes
-	const bytesSentPerSecond = sizeDataSentInBytes / deltaSec
-	const framesSentPerSecond = bytesSentPerSecond / singleFrameInBytes
-
-	const maxUsersByFramerate = []
-	for (let i = 1; i <= maxFramesPerSec; i++) {
-		const maxUsers = framesSentPerSecond / i
-		maxUsersByFramerate.push({ maxUsers, fps: i })
-	}
-
-	console.log({
-		singleFrameInBytes,
-		totalFramesSent,
-		durSec: deltaSec,
-		bytesSentPerSecond,
-		maxUsersByFramerate,
-	})
-
-	process.exit()
+		if (counter === totalFramesToSend) {
+			end1 = Date.now() - start
+			clearInterval(intervalId)
+			console.log('Done sending data.')
+			const settledPromises = await Promise.allSettled(promises)
+			const end2 = Date.now() - start
+			const rejectedPromises = settledPromises.filter(
+				(p) => p.status === 'rejected'
+			)
+			rejectedPromises.forEach((p) => console.log(p.reason))
+			console.log('All promises settled.')
+			saveLog(
+				targetFps,
+				runTimeMs,
+				end1,
+				end2,
+				counter,
+				rejectedPromises.length,
+				runTimeString
+			)
+			process.exit()
+		}
+	}, 1000 / targetFps)
 }
 
-writeUserData()
+writeUserData(targetFps, runTimeString)
