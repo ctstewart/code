96e56fe7cf43ba84a7a8f10aa33cc58f12460aaa
Christian Stewart <heeler_cs@hotmail.com>
2023-03-30 19:13:14 -0500

    Commented out unit test and added comments

--- convertTimeTextToMs.js
+++ convertTimeTextToMs.js
@@ -1,4 +1,4 @@
-// Options for time text: is, im, ih where i is the number and s, m, h is the unit
+// Options for time text: is, im, ih where i is a positive integer and s, m, h is for seconds, minutes, and hours respectively
 export const convertTimeTextToMs = (timeText) => {
 	const time = timeText.slice(0, -1)
 	const unit = timeText.slice(-1)
@@ -12,21 +12,19 @@ export const convertTimeTextToMs = (timeText) => {
 	return timeMs
 }
 
-const convertTimeTextToMsUnitTest = () => {
-	const timeText = '1s'
-	const timeMs = convertTimeTextToMs(timeText)
-	console.log('Expected: 1000')
-	console.log(timeMs)
+// const convertTimeTextToMsUnitTest = () => {
+// 	const timeText = '1s'
+// 	const timeMs = convertTimeTextToMs(timeText)
+// 	console.log('Expected: 1000')
+// 	console.log(timeMs)
 
-	const timeText2 = '1m'
-	const timeMs2 = convertTimeTextToMs(timeText2)
-	console.log('Expected: 60000')
-	console.log(timeMs2)
+// 	const timeText2 = '1m'
+// 	const timeMs2 = convertTimeTextToMs(timeText2)
+// 	console.log('Expected: 60000')
+// 	console.log(timeMs2)
 
-	const timeText3 = '1h'
-	const timeMs3 = convertTimeTextToMs(timeText3)
-	console.log('Expected: 3600000')
-	console.log(timeMs3)
-}
-
-convertTimeTextToMsUnitTest()
+// 	const timeText3 = '1h'
+// 	const timeMs3 = convertTimeTextToMs(timeText3)
+// 	console.log('Expected: 3600000')
+// 	console.log(timeMs3)
+// }
